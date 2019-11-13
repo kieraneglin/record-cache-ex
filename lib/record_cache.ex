af3dab5871a1a371321e5c1cf55e0cb1c6aca842
Kieran Eglin <kieran.eglin@gmail.com>
2019-11-12 22:25:07 -0800

    Hooked up CacheVersion module to main module

--- lib/record_cache.ex
+++ lib/record_cache.ex
@@ -1,12 +1,15 @@
 defmodule RecordCache do
-  alias RecordCache.CacheKey
+  alias RecordCache.{
+    CacheKey,
+    CacheVersion
+  }
 
   def cache_key(record) do
     CacheKey.call(record)
   end
 
   def cache_version(record) do
-    IO.inspect(record)
+    CacheVersion.call(record)
   end
 
   def cache_key_with_version(record) do
