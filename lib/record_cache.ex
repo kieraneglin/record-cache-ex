877f0311a8e08b9a007c6f6025b5af2478e47a62
Kieran Eglin <kieran.eglin@gmail.com>
2019-11-12 20:34:54 -0800

    Initial commit

---
+++ lib/record_cache.ex
@@ -0,0 +1,13 @@
+defmodule RecordCache do
+  def cache_key(record) do
+    IO.inspect(record)
+  end
+
+  def with_version(record) do
+    IO.inspect(record)
+  end
+
+  def cache_key_with_version(record) do
+    IO.inspect(record)
+  end
+end
---
+++ test/record_cache_test.exs
@@ -0,0 +1,8 @@
+defmodule RecordCacheTest do
+  use ExUnit.Case
+  doctest RecordCache
+
+  test "TODO" do
+    assert false
+  end
+end
